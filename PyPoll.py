d2c51659091a45d4c2b586827380e5c4fb39851d
adshaughn <96558619+adshaughn@users.noreply.github.com>
2022-01-17 09:10:43 -0700

    adding pypoll file

---
+++ PyPoll.py
@@ -0,0 +1,32 @@
+##Election Analysis
+
+#import dependencies
+
+import csv
+
+# assign a variable for the data file and open the path
+
+file_to_load = 'Resources/election_results.csv'
+
+# open the file and read the file
+
+with open(file_to_load) as election_data:
+
+    #To do : perform analysis
+    
+    print(election_data)
+
+# close the file
+
+election_data.close()
+
+
+#total number of votes cast
+
+#complete list of candidates who received votes
+
+#percentage of votes each candidate won
+
+#total number of votes each candidate won
+
+#winner of election based on popular vote
