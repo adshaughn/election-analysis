4f04959399b8b10856ee62cdd78f09c6d60412c7
adshaughn <96558619+adshaughn@users.noreply.github.com>
2022-01-17 21:54:42 -0700

    updates to pypoll

--- PyPoll.py
+++ PyPoll.py
@@ -11,21 +11,42 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# STEP ONE Initialize a total vote counter
+total_votes = 0
+
+# Declaring a new list
+candidate_options = []
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
 
-     # To do: read and analyze the data here
-
      # Read the file object with the reader function.
      file_reader = csv.reader(election_data)
      
-     # read and print the header row
+     # read the header row
      headers = next(file_reader)
-     print(headers)
+     
+     #print each row in the CSV file
+     for row in file_reader:
+          # STEP 2 Add to the total vote count
+          total_votes += 1
+
+          # Print the candidate name from each row
+          candidate_name = row[2]
+
+          #add unique candidate name to the candidate list
+          if candidate_name not in candidate_options:
+               candidate_options.append(candidate_name)
+
+# STEP 3 Print the total votes
+print(total_votes)
+
+# print the candidate list
+print(candidate_options)
 
 
 #total number of votes cast
+     #answer: 369711
 
 #complete list of candidates who received votes
 
