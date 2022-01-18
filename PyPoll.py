0b19d5afa28d40f5a5aa3761bbf3f49626964300
adshaughn <96558619+adshaughn@users.noreply.github.com>
2022-01-17 21:40:51 -0700

    Updating PyPoll.py file.

--- PyPoll.py
+++ PyPoll.py
@@ -1,21 +1,28 @@
 ##Election Analysis
 
 #import dependencies
+from email import header
 import os
 import csv
 
 # assign a variable for the data file and open the path
 file_to_load = os.path.join("Resources", "election_results.csv")
 
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+
 # Open the election results and read the file
 with open(file_to_load) as election_data:
 
-     # To do: perform analysis.
-     print(election_data)
+     # To do: read and analyze the data here
 
-# close the file
-
-election_data.close()
+     # Read the file object with the reader function.
+     file_reader = csv.reader(election_data)
+     
+     # read and print the header row
+     headers = next(file_reader)
+     print(headers)
 
 
 #total number of votes cast
